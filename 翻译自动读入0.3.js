c722e8aa0a43b2221e1926fd725dc50ba762d8c1
空白Sora <130253989+235KH@users.noreply.github.com>
2023-08-19 01:31:48 +0900

    Update 翻译自动读入0.3.js

--- 翻译自动读入0.3.js
+++ 翻译自动读入0.3.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         翻译自动读入
-// @namespace    https://chat.openai.com/c/a5d08583-0401-468a-9458-508d3fe5f7bf
-// @version      0.2
+// @namespace    https://github.com/235KH/Minna-de-Honyaku-Plugin
+// @version      0.3
 // @description  A script to add a button and print button names and URLs
 // @author       空白Sora
 // @match        https://www.dlsite.com/circle/translation/sound/*
